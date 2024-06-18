318f43ee10b173b28e6e598a339451c761a1168f
Pushkaraj <pushkarajy4@gmail.com>
2024-06-18 22:41:23 +0530

    Operators

---
+++ Operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    cout << ++a << endl; // Pre-increment -> first increment the value then use it
+
+    int b = 5;
+    cout << b++ << endl; // Post-increment -> first use the value then increment it
+    cout << b << endl;
+
+    int num = 10;
+    cout << --num << endl; // Pre-decrement
+
+    int age = 20;
+    cout << num-- << endl; // Post-decrement
+    cout << num;
+}
