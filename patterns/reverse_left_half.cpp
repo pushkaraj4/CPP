0c5ef3a9b8f550a5038bdc3ac69f7272e9890ed0
Pushkaraj <pushkarajy4@gmail.com>
2024-06-20 20:52:24 +0530

    .

--- patterns/reverse_left_half.cpp
+++ patterns/reverse_left_half.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     int n = 5;
     for (int row = 0; row < n; row++) {
-        for (int j = 0; j < n - row - 1; j++) {
+        for (int j = 0; j < row; j++) {
             cout << "  ";
         }
-        for (int k = 0; k <= row; k++) {
+        for (int k = n; k > row; k--) {
             cout << "* ";
         }
         cout << endl;
